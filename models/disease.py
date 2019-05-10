c2fae811b52947c75b18ddd33ac310a657f379c6
ChengIHsuan <cosine12621@gmail.com>
2019-05-11 00:53:13 +0800

    0511

--- models/disease.py
+++ models/disease.py
@@ -10,7 +10,7 @@ class Disease():
 
     ## 綜合搜尋
     def search_all(self, disease, county, township, types, names, star, indexes):
-        try:
+        # try:
             ## 保留搜尋條件
             reserved = Search().disease_reserved(disease, county, township, names, types, star)
             reserved.append(Search().search_disease(disease))
@@ -40,9 +40,9 @@ class Disease():
             if sql_where != '':
                 sql_where = 'WHERE ' + sql_where
             return Select().exclude_none_data(indexes, sql_where, reserved)
-        except BaseException as e:
-            print('search_all Exception' + e)
-            return  render_template('search.hml')
+        # except BaseException as e:
+        #     print('search_all Exception' + e)
+        #     return  render_template('search.hml')
 
 class Select():
 
@@ -52,7 +52,7 @@ class Select():
 
     ## 排除所選指標皆為-1之資料
     def exclude_none_data(self, indexes, sql_where, reserved):
-        try:
+        # try:
             ## 產生所有指標不能皆為-1之條件
             str = ''
             for index in indexes:
@@ -63,51 +63,128 @@ class Select():
             ## 將condition加上所有指標不能皆為-1之條件
             sql_where += ' AND (' + str + ')'
             return Select().select_normal(indexes, sql_where, reserved)
-        except BaseException as e:
-            print('exclude_none_data Exception' + e)
-            return render_template('search.html')
+        # except BaseException as e:
+        #     print('exclude_none_data Exception' + e)
+        #     return render_template('search.html')
 
     ## 取得醫療機構資訊
     def select_normal(self, indexes, sql_where, reserved):
-        try:
+        # try:
+            deno_substr = ""
+            for r in range(len(indexes)):
+                # 截取merge_data分母數字
+                deno = ("substr(d_" + indexes[r] + ", instr(d_" + indexes[r] + ", '：') + 1, length(d_" + indexes[
+                    r] + ") - instr(d_" + indexes[r] + ", '：') - 1)")
+                if indexes[r] == indexes[-1]:
+                    deno_substr += deno
+                else:
+                    deno_substr += (deno + ",")
+            # 把所選的指標的分母按星等排序
+            sqlstr = "SELECT h.id," + deno_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id order by cast(fr.star as float) DESC"
+            deno_all = self.cursor.execute(sqlstr).fetchall()
+
+            # 把醫院id存成list之後比對用
+            hos_id = []
+            # 存該醫院是否大於30
+            boolean = []
+            for k in range(len(deno_all)):
+                i = 0
+                hos_id.append(deno_all[k][0])
+                for r in range(len(indexes)):
+                    # 把沒分母的改成0
+                    if deno_all[k][r + 1] == "（此指標無確切就醫人數":
+                        new_deno = 0
+                    else:
+                        print(deno_all[k])
+                        # 把資料全部轉成整數
+                        new_deno = int(deno_all[k][r + 1])
+                    if new_deno < 30:
+                        i = i + 1
+                if i == len(indexes):
+                    boolean.append("None")
+                else:
+                    boolean.append("True")
+
+            # 合併醫院id跟true false
+            new_bool = []
+            for ab in zip(hos_id, boolean):
+                new_bool.append(ab)
+
+            # 存只有true的
+            True_only = []
+            # 存只有false的
+            False_only = []
+            for k in range(len(new_bool)):
+                if new_bool[k][1] == "True":
+                    True_only.append(new_bool[k])
+                else:
+                    False_only.append(new_bool[k])
+
+            # 合併成一個list
+            True_only.extend(False_only)
+
             ## select醫療機構資訊'：名稱、分數＆星等、正向評論數、負向評論數、電話與地址並存入normal[]
-            sqlstr = "SELECT h.abbreviation, cast(fr.star as float), fr.positive,  fr.negative, h.phone, h.address FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id  " + sql_where
-            normal = self.cursor.execute(sqlstr).fetchall()  #normal = [ (名稱1, GOOGLE星等1, 正向評論數1, 負向評論數1, 電話1, 地址1), ......]#
+            sqlstr = "SELECT  h.abbreviation, cast(fr.star as float), fr.positive,  fr.negative, h.phone, h.address , h.id FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id  " + sql_where + " order by cast(fr.star as float) DESC"
+            f = self.cursor.execute(sqlstr).fetchall()  ## normal = [ (名稱, GOOGLE星等, 正向評論數, 負向評論數, 電話, 地址), ......]
+            # 把分母排好的順序跟醫療機構資訊對醫院id
+            normal = []
+            for k in range(len(True_only)):
+                for n in range(len(f)):
+                    if True_only[k][0] == f[n][6]:
+                        normal.append(f[n])
             ## 若未找到任何資料，出現錯誤訊息，若有則進入else
             if normal == []:
                 alert = "抱歉，找不到您要的資料訊息。"
                 return render_template("search.html", alert=alert)
             else:
                 return Select().select_data(normal, indexes, sql_where, reserved)
-        except BaseException as e:
-            print('select_normal Exception' + e)
-            return render_template('search.html')
+        # except BaseException as e:
+        #     print('select_normal Exception' + e)
+        #     return render_template('search.html')
 
     ## 取得使用者勾選的資訊
     def select_data(self, normal, indexes, sql_where, reserved):
-        try:
+        # try:
             value_substr = 'm.hospital_id'
             deno_substr = 'm.hospital_id'
             level_substr = 'm.hospital_id'
             for r in range(len(indexes)):
-                value_substr += (', ' + 'm.v_' + indexes[r])
-                level_substr += (', ' + 'm.l_' + indexes[r])
-                deno_substr += (', ' + 'm.d_' + indexes[r])
+               value_substr += (', ' + 'm.v_' + indexes[r])
+               level_substr += (', ' + 'm.l_' + indexes[r])
+               deno_substr += (', ' + 'm.d_' + indexes[r])
             ## 取得data指標值
-            sqlstr = "SELECT " + value_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where
-            l_value = self.cursor.execute(sqlstr).fetchall()  ## l_value = [(hospital_id, 指標1之指標值, 指標2之指標值, 指標3之指標值, ......), ......]
+            sqlstr = "SELECT " + value_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where + " order by cast(fr.star as float) DESC"
+            g = self.cursor.execute(sqlstr).fetchall()  ## l_value = [(hospital_id, 指標1之指標值, 指標2之指標值, 指標3之指標值, ......), ......]
+            l_value = []
+            for k in range(len(normal)):
+                for n in range(len(g)):
+                    if normal[k][6] == g[n][0]:
+                        l_value.append(g[n])
+
             ## 取得data分母(就醫人數)
-            sqlstr = "SELECT " + deno_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where
-            l_deno = self.cursor.execute(sqlstr).fetchall()
+            sqlstr = "SELECT " + deno_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where + " order by cast(fr.star as float) DESC"
+            z = self.cursor.execute(sqlstr).fetchall()
+            l_deno = []
+            for k in range(len(normal)):
+                for n in range(len(z)):
+                    if normal[k][6] == z[n][0]:
+                        l_deno.append(z[n])
+
             ## 取得data指標值等級
-            sqlstr = "SELECT " + level_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where
-            l_level = self.cursor.execute(sqlstr).fetchall()
+            sqlstr = "SELECT " + level_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where + " order by cast(fr.star as float) DESC"
+            y = self.cursor.execute(sqlstr).fetchall()
+            l_level = []
+            for k in range(len(normal)):
+                for n in range(len(y)):
+                    if normal[k][6] == y[n][0]:
+                        l_level.append(y[n])
+
             ## 將醫療機構資訊、指標值、就醫人數、指標值等級包裝成zip
             z_data = zip(normal, l_value, l_deno, l_level)
             return Result().get_column_name(indexes, z_data, sql_where, reserved)
-        except BaseException as e:
-            print('select_data Exception' + e)
-            return render_template('search.html')
+        # except BaseException as e:
+        #     print('select_data Exception' + e)
+        #     return render_template('search.html')
 
 class Result():
 
@@ -117,44 +194,37 @@ class Result():
 
     ## 取得欄位名稱
     def get_column_name(self, indexes, z_data, sql_where, reserved):
-        try:
+        # try:
             ## 先取得欄位的原始名字(m.v_?)，「醫院機構資訊」為固定欄位，直接手動新增
             getColumns=['醫療機構資訊']
-            for r in range(len(indexes)):
-                getColumns.append(indexes[r])
+            for index in indexes:
+                getColumns.append(index)
             ## 建立columns[]，存入從資料庫中取得的欄位名稱(縮寫)
             columns = []
-            PorN = []
+            sort_indexes = []
             for c in getColumns:
-                columns.append(self.cursor.execute("SELECT abbreviation FROM column_name WHERE name = '" + str(c) + "'").fetchone()[0])
-                PorN.append(self.cursor.execute("SELECT PorN FROM column_name WHERE name = '" + str(c) + "'").fetchone()[0])
-            ## 建立full_name[]，存入欄位名稱(縮寫)的完整名字
-            full_name = ['醫療機構資訊']
-            for fn in columns:
-                if fn != '醫療機構資訊':
-                    full_name.append(self.cursor.execute("SELECT name FROM indexes WHERE abbreviation = '" + fn + "'").fetchone()[0])
-            ## 將欄位名稱、欄位詳細說明包裝成zip
-            z_col = zip(columns, PorN, full_name)
+                column = self.cursor.execute("SELECT abbreviation, PorN, description FROM column_name WHERE name = '" + str(c) + "'").fetchall()[0]
+                columns.append(column)
+                sort_indexes.append(column[0])
             ## 選取的指標數量，-1是因為扣掉第一欄的醫療機構資訊
             ck_len = len(columns) - 1
-            ## 取得使用者所選指標將放進排序選單中，第一個為醫療機構資訊，所以不取
-            sort_indexes = columns[1:]
-            return Result().table(z_data, z_col, ck_len, indexes, sql_where, sort_indexes, reserved)
-        except BaseException as e:
-            print('get_column_name Exception' + e)
-            return render_template('search.html')
+            ## sort_indexes第一個為【醫療機構資訊】，不列入排序選項
+            del sort_indexes[0]
+            return Result().table(z_data, columns, ck_len, indexes, sql_where, sort_indexes, reserved)
+        # except BaseException as e:
+        #     print('get_column_name Exception' + e)
+        #     return render_template('search.html')
 
     ## 將搜尋結果寫進表格
-    def table(self, z_data, z_col, ck_len, indexes, sql_where, sort_indexes, reserved):
-        try:
+    def table(self, z_data, columns, ck_len, indexes, sql_where, sort_indexes, reserved):
+        # try:
             tmp_indexes = ''
             for r in range(len(indexes)):
                 tmp_indexes += indexes[r] + '//'
             sql_where = sql_where.replace(' ', '//')
             z_indexes = zip(indexes, sort_indexes)
             ## render至前端HTML，ck_len為指標的長度，columns為欄位名稱，z為醫院資訊和指標值的zip
-            print(reserved)
-            return render_template('diseaseResult.html', reserved=reserved, ck_len=ck_len, z_col=z_col, z_data=z_data, sql_where=sql_where, tmp_indexes=tmp_indexes, z_indexes=z_indexes)
-        except BaseException as e:
-            print('table Exception' + e)
-            return render_template('search.html')
+            return render_template('diseaseResult.html', reserved=reserved, ck_len=ck_len, columns=columns, z_data=z_data, sql_where=sql_where, tmp_indexes=tmp_indexes, z_indexes=z_indexes)
+        # except BaseException as e:
+        #     print('table Exception' + e)
+        #     return render_template('search.html')
--- models/sort.py
+++ models/sort.py
@@ -19,59 +19,34 @@ class Sort():
             orderby = (" order by " + index_value + " DESC")
         elif P_or_N == 'N':  ## 負面指標升序排列
             orderby = (" order by " + index_level + " DESC, " + index_value + " ASC")
-        print(orderby)
-        return Sort().select_normal(sql_where, indexes, orderby, reserved)
-        # except:
-        #     alert = "請選擇排序指標"
-        #     return render_template("diseaseResult.html", alert=alert)
+        return Sort().select_data2(indexes, sql_where, orderby, reserved)
 
-    ## 加上所選標皆不為-1之條件
-    # def add_sql_where(self, sql_where, indexes, search_filter, orderby):
-    #     print('add')
-    #     try:
-    #         str = ''
-    #         for index in indexes:
-    #             if index != indexes[-1]:
-    #                 str += '(m.v_' + index + ' != -1) OR '
-    #             else:
-    #                 str += '(m.v_' + index + ' != -1)'
-    #         ## 將condition改回，並加上指標皆不為-1之條件
-    #         sql_where += ' AND (' + str + ')'
-    #         print(sql_where)
-    #         return Sort().select_normal(indexes, sql_where, search_filter, orderby)
-    #     except:
-    #         alert = "請選擇指標。"
-    #         return render_template('diseaseResult.html', alert=alert)
+    def form_substr(self, nvdl, indexes, sql_where, orderby):
+        nvdlStr = {
+            'normal': "h.abbreviation, cast(fr.star as float), fr.positive, fr.negative, h.phone, h.address",
+            'value': "m.v_",
+            'deno': "m.d_",
+            'level': "m.l_"
+        }
 
-    def select_normal(self, sql_where, indexes, orderby, reserved):
-        sqlstr = "SELECT h.abbreviation, cast(fr.star as float), fr.positive, fr.negative, h.phone, h.address FROM hospitals h JOIN final_reviews fr ON h.id = fr.hospital_id join merge_data m ON h.id = m.hospital_id " + sql_where + orderby
-        normal = self.cursor.execute(sqlstr).fetchall()
-        ## 若未找到任何資料，出現錯誤訊息，若有則進入else
-        if normal == []:
-            flash('抱歉，找不到您要的資料訊息。')
-            return render_template("diseaseResult.html")
+        if nvdl != 'normal':
+            substr = 'm.hospital_id'
+            for r in range(len(indexes)):
+                substr += (', ' + nvdlStr.get(nvdl) + indexes[r])
         else:
-            return Sort().select_data2(normal, indexes, sql_where, orderby, reserved)
+            substr = nvdlStr.get(nvdl)
 
-    def select_data2(self, normal, indexes, sql_where, orderby, reserved):
-        value_substr = 'm.hospital_id'
-        deno_substr = 'm.hospital_id'
-        level_substr = 'm.hospital_id'
-        for r in range(len(indexes)):
-            value_substr += (', ' + 'm.v_' + indexes[r])
-            level_substr += (', ' + 'm.l_' + indexes[r])
-            deno_substr += (', ' + 'm.d_' + indexes[r])
-        ## 取得data指標值
-        sqlstr = "SELECT " + value_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where + orderby
-        l_value = self.cursor.execute(sqlstr).fetchall()
-        ## 取得data分母(就醫人數)
-        sqlstr = "SELECT " + deno_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where + orderby
-        l_deno = self.cursor.execute(sqlstr).fetchall()
-        ## 取得data指標值等級
-        sqlstr = "SELECT " + level_substr + " FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id " + sql_where + orderby
-        l_level = self.cursor.execute(sqlstr).fetchall()
+        sqlstr = "SELECT {} FROM merge_data m JOIN hospitals h ON m.hospital_id = h.id JOIN final_reviews fr ON h.id = fr.hospital_id ".format(substr) + sql_where + orderby
+        data = self.cursor.execute(sqlstr).fetchall()
+        return data
+
+    def select_data2(self, indexes, sql_where, orderby, reserved):
+        normal = Sort().form_substr('normal', indexes, sql_where, orderby)
+        value = Sort().form_substr('value', indexes, sql_where, orderby)
+        deno = Sort().form_substr('deno', indexes, sql_where, orderby)
+        level = Sort().form_substr('level', indexes, sql_where, orderby)
         ## 將醫療機構資訊、指標值、就醫人數、指標值等級包裝成zip
-        z_data = zip(normal, l_value, l_deno, l_level)
+        z_data = zip(normal, value, deno, level)
         return Result().get_column_name(indexes, sql_where, z_data, reserved)
 
 class Result():
@@ -84,34 +59,28 @@ class Result():
     def get_column_name(self, indexes, sql_where, z_data, reserved):
         ## 先取得欄位的原始名字(m.d.?)，「醫院機構資訊」為固定欄位，直接手動新增
         getColumns = ['醫療機構資訊']
-        for r in range(len(indexes)):
-            getColumns.append(indexes[r])
+        for index in indexes:
+            getColumns.append(index)
         ## 建立columns[]，存入從資料庫中取得的欄位名稱(縮寫)
         columns = []
-        PorN = []
+        sort_indexes = []
         for c in getColumns:
-            columns.append(self.cursor.execute("SELECT abbreviation FROM column_name WHERE name = '" + c + "'").fetchone()[0])
-            PorN.append(self.cursor.execute("SELECT PorN FROM column_name WHERE name = '" + str(c) + "'").fetchone()[0])
-        ## 建立full_name[]，存入欄位名稱(縮寫)的完整名字
-        full_name = ['醫療機構資訊']
-        for fn in columns:
-            if fn != '醫療機構資訊':
-                full_name.append(self.cursor.execute("SELECT name FROM indexes WHERE abbreviation = '" + fn + "'").fetchone()[0])
-        ## 將欄位名稱、欄位詳細說明包裝成zip
-        z_col = zip(columns, PorN, full_name)
+            column = self.cursor.execute("SELECT abbreviation, PorN, description FROM column_name WHERE name = '" + str(c) + "'").fetchall()[0]
+            columns.append(column)
+            sort_indexes.append(column[0])
         ## 選取的指標數量，-1是因為扣掉第一欄的醫療機構資訊
         ck_len = len(columns) - 1
-        sort_indexes = columns[1:]
-        return Result().table(z_data, z_col, ck_len, indexes, sql_where, sort_indexes, reserved)
+        ## sort_indexes第一個為【醫療機構資訊】，不列入排序選項
+        del sort_indexes[0]
+        return Result().table(z_data, columns, ck_len, indexes, sql_where, sort_indexes, reserved)
 
     ## 將搜尋結果寫進表格
-    def table(self, z_data, z_col, ck_len, indexes, sql_where, sort_indexes, reserved):
+    def table(self, z_data, columns, ck_len, indexes, sql_where, sort_indexes, reserved):
         tmp_indexes = ''
         for r in range(len(indexes)):
             tmp_indexes += indexes[r] + '//'
         sql_where = sql_where.replace(' ', '//')
         z_indexes = zip(indexes, sort_indexes)
         reserved = (reserved[1:-1].replace("'", "")).split(', ')
-        print(reserved)
         ## render至前端HTML，ck_len為指標的長度，columns為欄位名稱，z為醫院資訊和指標值的zip
-        return render_template('diseaseResult.html', scroll='results', reserved=reserved, ck_len=ck_len, z_col=z_col, z_data=z_data, sql_where=sql_where, tmp_indexes=tmp_indexes, z_indexes=z_indexes)
+        return render_template('diseaseResult.html', scroll='results', reserved=reserved, ck_len=ck_len, columns=columns, z_data=z_data, sql_where=sql_where, tmp_indexes=tmp_indexes, z_indexes=z_indexes)
